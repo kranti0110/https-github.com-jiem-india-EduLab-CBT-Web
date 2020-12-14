77f43fb19804658327847b2190d30ad23079f5e4
amrutabisain1 <72014041+amrutabisain1@users.noreply.github.com>
2020-12-14 16:05:11 +0530

    Assert file for Exam management
    Assert file for Exam management

--- src/main/java/oneup/report/Assert.java
+++ src/main/java/oneup/report/Assert.java
@@ -2,10 +2,23 @@ package oneup.report;
 
 public class Assert {
 
+	@SuppressWarnings("deprecation")
 	public static void assertEquals(String expected , String actual, String stepName) {
 		if(expected == null) {expected = "null";}
 		if(actual == null) {actual = "null";}
 		//Reporter.addValidationStep(stepName, "", expected.equals(actual));
 		junit.framework.Assert.assertEquals(expected, actual);
+	}
+
+	@SuppressWarnings("deprecation")
+	public static void assertFalse(Boolean condition) {
+		//Reporter.addValidationStep(stepName, "", expected.equals(actual));
+		junit.framework.Assert.assertFalse(condition);
+	}
+	
+	@SuppressWarnings("deprecation")
+	public static void assertTrue(Boolean condition) {
+		//Reporter.addValidationStep(stepName, "", expected.equals(actual));
+		junit.framework.Assert.assertTrue(condition);
 	}
 }
